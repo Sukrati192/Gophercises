283bf6d661692df11cf0b747b1f0ad2ebdd7f489
sukrati <sukrati.agrawal@exotel.in>
2021-11-22 23:05:27 +0530

    Add conditions to handle text with newline

--- html_link_parser/parser.go
+++ html_link_parser/parser.go
@@ -29,116 +29,56 @@ func ParseLinkAndTextFromHtml(inputfile string) []Links {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var links []Links
-	links = getTextAndLinkFromHtml(doc)
+	links := []Links{}
+	links = getTextAndLinkFromHtml(doc, links)
 	return links
 }
 
-func isAnchorTag(n *html.Node) bool {
-	return n.Type == html.ElementNode && n.Data == "a"
-}
-
-func getTextAndLinkFromHtml(n *html.Node) []Links {
-	links := []Links{}
-	if isAnchorTag(n) {
-		for _, attr := range n.Attr {
-			if attr.Key == "href" {
-				textArr := []string{}
+func getTextAndLinkFromHtml(n *html.Node, links []Links) []Links {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				text := ""
 				for child := n.FirstChild; child != nil; child = child.NextSibling {
-					if child == nil {
-						break
-					}
-
-					t := addText(child)
-					if t == nil {
-						continue
-					}
-					textArr = append(textArr, *t)
+					text = addText(child, text)
 				}
-				text := strings.Join(textArr, "")
-				link := Links{attr.Val, strings.TrimSpace(text)}
+				link := Links{a.Val, strings.TrimSpace(text)}
 				links = append(links, link)
 			}
 		}
 		return links
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		links = append(links, getTextAndLinkFromHtml(c)...)
+		links = getTextAndLinkFromHtml(c, links)
 	}
 	return links
 }
 
-func addText(n *html.Node) *string {
+func addText(n *html.Node, text string) string {
 	if n == nil {
-		return nil
+		if strings.TrimSpace(text) == text {
+			return text
+		}
+		return (strings.TrimSpace(text) + " ")
 	}
 	if n.Type == html.TextNode {
-		str := n.Data
-		var frontSpacesStrippedText string
-		if frontSpacesStrippedText = str; strings.HasPrefix(str, "\n") {
-			frontSpacesStrippedText = strings.TrimLeft(str, "\n ")
+		data := n.Data
+		if strings.TrimSpace(text) != text {
+			text = strings.TrimSpace(text) + " "
 		}
-
-		spacesStrippedText := strings.TrimRight(frontSpacesStrippedText, "\n ")
-
-		if spacesStrippedText == frontSpacesStrippedText {
-			// Nothing after text chars
-			return &spacesStrippedText
+		if strings.TrimSpace(n.Data) != n.Data {
+			data = strings.TrimSpace(n.Data) + " "
 		}
-
-		retVal := spacesStrippedText
-		for i := len(spacesStrippedText); i < len(frontSpacesStrippedText) && frontSpacesStrippedText[i] != byte(10); i++ {
-			retVal += string(frontSpacesStrippedText[i])
-		}
-
-		return &retVal
+		return text + data
 	}
-	return addText(n.FirstChild)
+	return addText(n.FirstChild, text)
 }
 
 func readFile(inputFile string) io.Reader {
-	fileName := fmt.Sprint("./examples/", inputFile)
+	fileName := fmt.Sprint("/home/sukrati/Gophercises/html_link_parser/examples/", inputFile)
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalln("Coudn't open input file. Error: ", err)
 	}
 	return file
 }
-
-/***
-- This is how the text nodes look like for various test cases: 2, 3, 4, 7, 8
-- I have printed string, size, json encoded string (to see all chars)
-- The pattern is this, if string starts with \n strip all spaces from front
-- Strip everything folowing the first \n after last char in text Eg. "chck \n    " -> "chck "
-
-
-A link to another page 22 "A link to another page"
-
-            Check me out on twitter
-             49 "\n            Check me out on twitter\n            "
-
-         9 "\n        "
-
-            Gophercises is on  31 "\n            Gophercises is on "
-Github 6 "Github"
-!
-         10 "!\n        "
-Login  6 "Login "
-Lost? Need help? 16 "Lost? Need help?"
-@marcusolsson 13 "@marcusolsson"
-dog cat
-         16 "dog cat\n        "
-
-     5 "\n    "
-text inside dog link 20 "text inside dog link"
-
-     5 "\n    "
-Something in a span 19 "Something in a span"
- Text not in a span
-     24 " Text not in a span\n    "
-Bold text! 10 "Bold text!"
-
- 1 "\n"
-
-
-*/
